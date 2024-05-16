a376f1e01b61f74fec8b16796fc606cb0ba9def9
ChenShihao <chenshihao@stu.xmu.edu.cn>
2024-05-16 16:54:33 +0800

    check0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -7,59 +7,68 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return is_closed_;
 }
 
 void Writer::push( string data )
 {
   // Your code here.
-  (void)data;
+  if (is_closed())
+    return;
+  uint64_t available_capacity = this -> available_capacity();
+  uint64_t push_size = min(data.size(), available_capacity);
+  bytes_pushed_ += push_size;
+  buffer_ += data.substr(0, push_size);
   return;
 }
 
 void Writer::close()
 {
   // Your code here.
+  is_closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return (is_closed_ && this -> bytes_buffered() == 0);
 }
 
 uint64_t Reader::bytes_popped() const
 {
   // Your code here.
-  return {};
+  return bytes_popped_;
 }
 
 string_view Reader::peek() const
 {
   // Your code here.
-  return {};
+  string_view sv( buffer_.data(), min(static_cast<uint64_t>(1000), this->bytes_buffered()));
+  return sv;
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  uint64_t pop_size = min(len, buffer_.size());
+  bytes_popped_ += pop_size;
+  buffer_.erase(buffer_.begin(), buffer_.begin() +len);
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return buffer_.size();
 }
